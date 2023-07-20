4ee69ac48867676904b3fae861f037b96ffb3eea
Benarba Tewfik <tewfikhotspur@gmail.com>
2023-07-20 19:29:33 +0200

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(catName) {
+  return cats.push(catName);
+}
+function destructivelyPrependCat(catName) {
+  return cats.unshift(catName);
+}
+function destructivelyRemoveLastCat() {
+  return cats.pop();
+}
+function destructivelyRemoveFirstCat() {
+  return cats.shift();
+}
+function appendCat(name) {
+  return [...cats, name];
+}
+function prependCat(name) {
+  return [name, ...cats];
+}
+function removeLastCat() {
+  return cats.slice(0, -1);
+}
+function removeFirstCat() {
+  return cats.slice(1);
+}
